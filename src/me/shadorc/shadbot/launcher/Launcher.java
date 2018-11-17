7a32a4b0481fdab329fd76d7814cd778e7581d02
David <bi-crosseur@hotmail.fr>
2018-11-17 16:03:12 +0100

    Rename Main to Launcher and add logger

--- src/me/shadorc/shadbot/launcher/Launcher.java
+++ src/me/shadorc/shadbot/launcher/Launcher.java
@@ -6,88 +6,91 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.sun.management.OperatingSystemMXBean;
 
-public class Main {
+public class Launcher {
 
-	private static final float GB_RAM_TO_START = 4.5f;
-
+	private static final Logger LOG = LoggerFactory.getLogger(Launcher.class);
 	private static final float GB = 1024 * 1024 * 1024;
 	private static final OperatingSystemMXBean OS_BEAN =
 			(com.sun.management.OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
-	private static final Runtime RUNTIME = Runtime.getRuntime();
+
+	private static final float GB_RAM_TO_START = 4.5f;
 
 	private static String jarPath;
 	private static Process process;
 
 	public static void main(String[] args) {
 		if(args.length != 1) {
-			System.err.println("You need to specify only the path to the jar file.");
+			LOG.error("You need to specify only the path to the jar file.");
 			System.exit(ExitCode.FATAL_ERROR.value());
 		}
 
 		jarPath = args[0];
 
-		System.out.println(String.format("Available processors: %d cores", RUNTIME.availableProcessors()));
-		System.out.println(String.format("Total physical memory size: %.2f GB", Main.getTotalRam()));
-		System.out.println(String.format("Free physical memory size: %.2f GB", Main.getFreeRam()));
+		LOG.info(String.format("Available processors: %d cores", Runtime.getRuntime().availableProcessors()));
+		LOG.info(String.format("Total physical memory size: %.2f GB", Launcher.getTotalRam()));
+		LOG.info(String.format("Free physical memory size: %.2f GB", Launcher.getFreeRam()));
 
 		final ScheduledExecutorService scheduledThreadPool = Executors.newSingleThreadScheduledExecutor();
-		scheduledThreadPool.scheduleAtFixedRate(Main::watchRam, 5, 5, TimeUnit.MINUTES);
+		scheduledThreadPool.scheduleAtFixedRate(Launcher::watchRam, 5, 5, TimeUnit.MINUTES);
 
-		Main.loop();
+		Launcher.loop();
 	}
 
 	private static void loop() {
 		ExitCode exitCode;
 		do {
-			exitCode = Main.start();
-			System.out.println(String.format("Exit code: %s", exitCode.toString()));
+			exitCode = Launcher.start();
+			LOG.info(String.format("Exit code: %s", exitCode.toString()));
 		} while(exitCode.equals(ExitCode.RESTART));
 	}
 
 	private static ExitCode start() {
-		System.out.println("Starting...");
+		LOG.info("Starting...");
 
-		if(Main.getFreeRam() < GB_RAM_TO_START) {
-			System.err.println(String.format(
+		if(Launcher.getFreeRam() < GB_RAM_TO_START) {
+			LOG.error(String.format(
 					"Free physical memory insufficient to start the process (minimum required: %.1f GB)", GB_RAM_TO_START));
 			return ExitCode.FATAL_ERROR;
 		}
 
 		try {
 			// Allocate as much memory as possible leaving 500 MB free
-			final int allocatedRam = (int) (Math.ceil(Main.getFreeRam() - 0.5f) * 1000d);
+			final int allocatedRam = (int) (Math.ceil(Launcher.getFreeRam() - 0.5f) * 1000d);
 			final String xmx = String.format("-Xmx%dm", allocatedRam);
 			process = new ProcessBuilder("java", "-jar", xmx, jarPath).inheritIO().start();
 
-			System.out.println(String.format("Process started (PID: %d) with: %s",
+			LOG.info(String.format("Process started (PID: %d) with: %s",
 					process.pid(), process.info().commandLine().orElse("")));
 
 			return ExitCode.valueOf(process.waitFor());
 
 		} catch (IOException | InterruptedException err) {
-			System.err.println(String.format("An error occurred while starting the process: %s", err.getMessage()));
+			LOG.error(String.format("An error occurred while starting the process: %s", err.getMessage()));
 			err.printStackTrace();
 			return ExitCode.FATAL_ERROR;
 		}
 	}
 
 	private static void watchRam() {
-		System.out.println(String.format("RAM available: %.1f GB", Main.getFreeRam()));
-		if(Main.getFreeRam() < 0.75f) {
-			System.err.println("The available RAM is too low, restarting process...");
+		LOG.info(String.format("RAM available: %.1f GB", Launcher.getFreeRam()));
+		if(Launcher.getFreeRam() < 0.75f) {
+			LOG.error("The available RAM is too low, restarting process...");
 			process.destroy();
 			try {
 				if(!process.waitFor(10, TimeUnit.SECONDS)) {
-					System.err.println("The process has not been cleanly destroyed, killing the process forcibly...");
+					LOG.error("The process has not been cleanly destroyed, killing the process forcibly...");
 					process.destroyForcibly();
 				}
 			} catch (InterruptedException err) {
-				System.err.println("An error occurred while waiting for the process to finish.");
+				LOG.error("An error occurred while waiting for the process to finish.");
 				err.printStackTrace();
 			}
-			Main.loop();
+			Launcher.loop();
 		}
 	}
 
