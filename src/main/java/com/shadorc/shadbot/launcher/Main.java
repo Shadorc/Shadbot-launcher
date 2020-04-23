0914fbc5bc7696d088e4679bb8cf48f7bd977b32
David <bi-crosseur@hotmail.fr>
2020-04-23 13:42:28 +0200

    Reduce default ram needed to 3GB instead of 4.5GB

--- src/main/java/com/shadorc/shadbot/launcher/Main.java
+++ src/main/java/com/shadorc/shadbot/launcher/Main.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 
 public class Main {
 
+    public static final float DEFAULT_GB_MIN = 3f;
+    public static final int DEFAULT_RESTART_PERIOD = -1;
+
     public static void main(String[] args) {
         if (args.length == 1) {
             if ("-help".equals(args[0])) {
@@ -30,8 +33,8 @@ public class Main {
         }
 
         final String jarPath = Main.getJarPath();
-        final float gbMin = Float.parseFloat(System.getProperty("gbMin", "4.5"));
-        final int restartPeriod = Integer.parseInt(System.getProperty("restartPeriod", "-1"));
+        final float gbMin = Float.parseFloat(System.getProperty("gbMin", Float.toString(DEFAULT_GB_MIN)));
+        final int restartPeriod = Integer.parseInt(System.getProperty("restartPeriod", Integer.toString(DEFAULT_RESTART_PERIOD)));
 
         if (jarPath == null) {
             Utils.LOGGER.error("File not found. You can specify the path as an argument using '-Dfile=${file}'.");
